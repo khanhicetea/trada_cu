ce055c055a3dfdd82fd2524f4c0a174e3c5ed731
KhanhIceTea <khanhicetea@gmail.com>
2016-04-13 17:55:53 +0700

    Fix DB Charset

--- app/config/database.php
+++ app/config/database.php
@@ -10,7 +10,7 @@ return array(
                 'database'  => env('DB_DATABASE', 'db'),
                 'username'  => env('DB_USERNAME', 'root'),
                 'password'  => env('DB_PASSWORD', ''),
-                'charset'   => env('DB_CHARSET', 'utf-8'),
+                'charset'   => env('DB_CHARSET', 'utf8'),
                 'collation' => env('DB_COLLATION', 'utf8_unicode_ci'),
                 'prefix'    => '',
                 'logging'   => false,
